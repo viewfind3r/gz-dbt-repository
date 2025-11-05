ad91f756ecc8089843d432c624c14fd913674ac1
Nirmal Ramakrishnan <tetrahiker@gmail.com>
2025-11-05 13:46:13 +0000

    Added primary key tests

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -12,7 +12,7 @@ renamed as (
         orders_id,
         shipping_fee,
         logcost,
-        CAST(ship_cost AS FLOAT64)
+        CAST (ship_cost AS FLOAT64) AS ship_cost
 
     from source
     WHERE shipping_fee=shipping_fee_1
