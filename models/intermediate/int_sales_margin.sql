8c0a09efe3cae4aeb23779014c1dfa67b7141dae
Nirmal Ramakrishnan <tetrahiker@gmail.com>
2025-11-05 20:35:43 +0000

    sales margin simple code

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,10 +1,13 @@
-select 
-    products_id,
-    quantity,
-    purchase_price,
-    quantity*purchase_price AS purchase_cost,
-    revenue,
-    revenue-(quantity*purchase_price) AS margin
-from {{ ref('stg_raw__sales') }}
-Join {{ ref('stg_raw__product') }}
-Using (products_id)
+  SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
+
