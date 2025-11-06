2b365c70dd9d202addde137fd2b731e74d7ab23a
Nirmal Ramakrishnan <tetrahiker@gmail.com>
2025-11-06 22:16:03 +0000

    macro correction

--- macros/functions.sql
+++ macros/functions.sql
@@ -1,3 +1,3 @@
 {%macro margin_percent(revenue, purchase_cost, precision=2)%}
-    ROUND (SAFE_DIVIDE ({{revenue}-{purchase_cost}},{{revenue}}, {{precision}}
+    ROUND (SAFE_DIVIDE ({{revenue}-{purchase_cost}},{{revenue}}, {{precision}})
     {%endmacro%}
