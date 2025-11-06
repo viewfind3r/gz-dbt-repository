dcbb11e950b114f1019d576cd5f07714672a1fa9
Nirmal Ramakrishnan <tetrahiker@gmail.com>
2025-11-06 22:15:01 +0000

    macro

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -7,6 +7,7 @@
       purchase_price,
       ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
       ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+      {{ margin_percent('s.revenue', 's.quantity * CAST(p.purchase_price AS FLOAT64)') }}      
   FROM {{ref("stg_raw__sales")}} s
   LEFT JOIN {{ref("stg_raw__product")}} p
       USING (products_id)
