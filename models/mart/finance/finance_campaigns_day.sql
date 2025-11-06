c445fd3e81a0e9b8ec1d6e49e576bf80196e6fcc
Nirmal Ramakrishnan <tetrahiker@gmail.com>
2025-11-06 14:56:55 +0000

    finance_campaign_days mart

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,18 @@
+SELECT
+    date_date AS date,
+    operational_margin-ads_cost AS ads_margin,
+    operational_margin,
+    ads_cost,
+    impression AS ads_impression,
+    click AS clicks,
+    quantity,
+    revenue,
+    purchase_cost,
+    margin,
+    shipping_fee,
+    log_cost,
+    ship_cost
+FROM {{ ref('int_campaigns_day') }}
+JOIN {{ ref('finance_days') }}
+USING (date_date)
+ORDER BY date_date DESC
