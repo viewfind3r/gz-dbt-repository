9d1a984a81ae4965760d1232a7698432da914ef6
Nirmal Ramakrishnan <tetrahiker@gmail.com>
2025-11-06 22:23:14 +0000

    macro error 3

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -6,7 +6,7 @@
       quantity,
       purchase_price,
       ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin,
       {{ margin_percent('s.revenue', 's.quantity * CAST(p.purchase_price AS FLOAT64)') }}      
   FROM {{ref("stg_raw__sales")}} s
   LEFT JOIN {{ref("stg_raw__product")}} p
